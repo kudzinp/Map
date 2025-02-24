4d9a54e33c0c302d99a0948bf514ec4ff8eb0200
kudzinp <gavrilkoo@gmail.com>
2025-02-24 16:59:50 +0100

    initial commit

---
+++ Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by Pavel Kudin on 24/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Pavel Kudin on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
